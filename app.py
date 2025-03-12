8f7e3f29ee2f182d9c6f071ca0b8d2a7536bb360
Daniel Salas Alonso <dsa069@inlumine.ual.es>
2025-03-12 10:58:46 +0100

    SHITTY WEB

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
 import numpy as np
-import pickle
+import json
 
 # Cargar modelo entrenado
 modelo_path = "models/modelo.keras"
 modelo = tf.keras.models.load_model(modelo_path)
 
 # Cargar tokenizer entrenado
-with open("models/tokenizer.pkl", "rb") as f:
-    tokenizer = pickle.load(f)
-
+with open("models/tokenizer.json", "r") as f:
+    tokenizer_data = json.load(f)
+    
 app = Flask(__name__)
 
 @app.route("/")  # Ruta raíz
 def home():
-    return render_template('index.html')
+    return render_template('app.html')
 
 @app.route('/evaluar', methods=['POST'])
 def evaluar_opinion():
